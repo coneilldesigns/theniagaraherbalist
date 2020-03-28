b1ab1b322ca9319a07b6d6ebf7b429d00720ecde
Conor O'Neill <coneill.designs@gmail.com>
2020-03-28 12:07:06 -0400

    Adding loading to menu page

--- page-menu.php
+++ page-menu.php
@@ -5,6 +5,12 @@ $image = get_field( "header_image" );
 ?>
 
 <div id="menu-holder">
+
+  <div class="center-center text-center">
+    <img width="60" class="leaf mb-2" src="<?php echo $images_url; ?>leaf.svg" />
+    <p>Live Menu Loading...</p>
+  </div>
+
   <script async="" id="dutchie--embed__script" src="https://dutchie.com/api/v2/embedded-menu/5e41cc73451ecb007bd05cae.js"></script>
 </div>
 
